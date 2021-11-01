b7f46c5e5afa95d51b00e0f3a7731e0b594e605e
Paul Brubaker <pbrubak@gmail.com>
2021-11-01 15:50:52 -0700

    baby meme rendering

--- src/components/RandomColor.jsx
+++ src/components/RandomColor.jsx
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 
 export class RandomColor extends Component {
     render() {
+        const { color, babyMeme } = this.props;
         return (
             <div
                 className="colorful-square"
-                style={{
-                    width: '500px',
-                    height: '500px',
-                    backgroundColor: this.props.color,
-                }}
+                style={
+                    this.props.color[4] !== '9'
+                        ? {
+                              width: '500px',
+                              height: '500px',
+                              backgroundColor: color,
+                          }
+                        : {
+                              width: '500px',
+                              height: '500px',
+                              backgroundImage: `url(${babyMeme})`,
+                          }
+                }
             ></div>
         );
     }
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { RandomColor } from '../RandomColor';
+import babyMeme from '../../images/baby-meme.jpg';
 
 export default class App extends Component {
     state = { color: 'white' };
 
     componentDidMount() {
-        setInterval(
+        this.timerID = setInterval(
             () => this.setState({ color: this.getRandomColor() }),
             1000
         );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
     }
 
     getRandomColor() {
@@ -17,6 +22,6 @@ export default class App extends Component {
     }
 
     render() {
-        return <RandomColor color={this.state.color} />;
+        return <RandomColor color={this.state.color} babyMeme={babyMeme} />;
     }
 }
